4844b9598333edf27114b31f9a9a76d17b82a040
Volday <41511002+Volday@users.noreply.github.com>
2020-06-25 10:15:04 +0800

    EdgeScreenTeleport

---
+++ Assets/Scripts/EdgeScreenTeleport.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeScreenTeleport : MonoBehaviour
+{
+    private Camera mainCamera;
+    private void Awake()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        float screenRatio = (float)Screen.width / Screen.height;
+
+        //Телепорт по вертикали экрана
+        if (transform.position.z > mainCamera.orthographicSize) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - mainCamera.orthographicSize * 2);
+        }
+        if (transform.position.z < -mainCamera.orthographicSize)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + mainCamera.orthographicSize * 2);
+        }
+        //Телепорт по горизонтали экрана
+        float horizontalOrthographicSize = mainCamera.orthographicSize * screenRatio;
+        if (transform.position.x > horizontalOrthographicSize)
+        {
+            transform.position = new Vector3(transform.position.x - horizontalOrthographicSize * 2, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -horizontalOrthographicSize)
+        {
+            transform.position = new Vector3(transform.position.x + horizontalOrthographicSize * 2, transform.position.y, transform.position.z);
+        }
+    }
+}
